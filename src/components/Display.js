a3400602e2440b9ff59ca30a7f0798ae9e68a603
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-31 14:34:11 -0500

    display module css

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,8 @@
+import styles from "./Display.module.css"
+
 function Display(props){
     console.log("Display Rendered")
-    return <h1>{props.displayValue}</h1>
+    return <h1 className={styles.display}>{props.displayValue}</h1>
 }
 
 export default Display

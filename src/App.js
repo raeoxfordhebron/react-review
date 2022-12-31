5075d9ed3acdf6d391cf01c7abdd6f6711a53030
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-31 12:36:14 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+let count = 0;
+function countUp(){
+  count += 1
+  console.log(count)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={countUp}>Add One</button>
     </div>
   );
 }
